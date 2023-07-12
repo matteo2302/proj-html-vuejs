07dce451640ce6fb5223beedcf41c975c10c7188
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-12 20:44:38 +0200

    buy avada

--- src/components/AppBuyAvada.vue
+++ src/components/AppBuyAvada.vue
@@ -5,8 +5,8 @@ export default{}
 <div class="avada-cont">
     <div class="shadow ">
         <div class="container d-flex flex-column justify-content-center text-center"><h1>"i've failed over and over again in my life. And that is why I succed."</h1>
-        <p>^^^</p>
-        <p>Arcu auctor nisl, congue sit nisi tincidunt eget proin. In lacinia lactus donec sed massa in ipsum eros, tristique. Gravida suspendisse etiam in iaculis</p>
+        <p><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i></p>
+        <p class="color-grey">Arcu auctor nisl, congue sit nisi tincidunt eget proin. In lacinia lactus donec sed massa in ipsum eros, tristique. Gravida suspendisse etiam in iaculis</p>
         <div class="btn-position"><button>Buy avada today ></button></div>
         </div>
         
@@ -21,14 +21,25 @@ export default{}
     background-position: center;
     background-size: cover;
 }
+h1{
+    font-size: 45px;
+}
+.color-grey{
+    color: #98939F;
+}
 .shadow{
-    background-image:linear-gradient(to top, #4154ff54 5%,transparent);
+    background-image:linear-gradient(to top, black 5%,transparent);
     height: 700px;
 }
 .container{
     width: 700px;
     height: 700px;
     color:white
+}
+i{
+    color: #FFF941;
+    margin: -3px;
+    font-size: 25px;
 }
 button{
     padding: 20px 0;
