832414bfc3c0b1722e25e6683c437ba4a0dd4034
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-12 23:13:38 +0200

    sponsor

--- src/components/AppSponsor.vue
+++ src/components/AppSponsor.vue
@@ -1,49 +1,57 @@
 <script>
-export default{}
+export default {}
 </script>
-<template> 
-<div class="d-flex justify-content-between container">
-    <div class="cardsponsor d-flex flex-column justify-content-center mx-4 text-center">
-    <img src="../assets/sponsor-1-2x.png" alt="Yoga Studio"/>
-    <p>^^^</p>
-    <p>tristique aliquaman in nullam habitasse nunc, amet vel consectetur. Nulla in suspendisse dolor, aliquet.</p>
-    <a href="#">Visit Yoga Studio ></a>
-   </div>
-   <div class="cardsponsor d-flex flex-column justify-content-center mx-4 text-center">
-    <img src="../assets/sponsor-2-2x.png" alt="Fitnes Center"/>
-    <p>^^^</p>
-    <p>Feugiat etiam dui mauris pharetra mauris sed pharetra. Convallis sapien ornare cras faucibus nulla porta dui a.</p>
-    <a href="#">Join Fitness Center ></a>
-   </div>
-   <div class="cardsponsor d-flex flex-column justify-content-center mx-4 text-center">
-    <img src="../assets/sponsor-3-2x.png" alt="Fitness Club"/>
-    <p>^^^</p>
-    <p>Nist at adpiscing, nec, adipiscing commodo aliquam tincidunt. Ligula in habitant imperdient</p>
-    <a href="#"> Learn about FC ></a>
-   </div>
-   <div class="cardsponsor d-flex flex-column justify-content-center mx-4 text-center">
-    <img src="../assets/sponsor-4-2x.png" alt="Power gym"/>
-    <p>^^^</p>
-    <p>Proin in viverra Maecenas et mattis ut nibh enim. Gravida ultricies mi porta lacus. Et posuere velit sed nec eleifend</p>
-    <a href="#"> Visit Power Gym ></a>
-   </div>
-   
-</div>
+<template>
+    <div class="d-flex justify-content-between align-items-center container">
+        <div class="cardsponsor d-flex flex-column justify-content-center mx-4 align-items-center text-center">
+            <img src="../assets/sponsor-1-2x.png" alt="Yoga Studio" />
+            <p class="my-3"><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i></p>
+            <p>tristique aliquaman in nullam habitasse nunc, amet vel consectetur. Nulla in suspendisse dolor, aliquet.</p>
+            <a class="my-3" href="#">Visit Yoga Studio ></a>
+        </div>
+        <div class="cardsponsor d-flex flex-column justify-content-center mx-4 align-items-center text-center">
+            <img src="../assets/sponsor-2-2x.png" alt="Fitnes Center" />
+            <p class="my-3"><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i></p>
+            <p>Feugiat etiam dui mauris pharetra mauris sed pharetra. Convallis sapien ornare cras faucibus nulla porta dui
+                a.</p>
+            <a class="my-3" href="#">Join Fitness Center ></a>
+        </div>
+        <div class="cardsponsor d-flex flex-column justify-content-center mx-4 align-items-center text-center">
+            <img src="../assets/sponsor-3-2x.png" alt="Fitness Club" />
+            <p class="my-3"><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i></p>
+            <p>Nist at adpiscing, nec, adipiscing commodo aliquam tincidunt. Ligula in habitant imperdient</p>
+            <a class="my-3" href="#"> Learn about FC ></a>
+        </div>
+        <div class="cardsponsor d-flex flex-column justify-content-center mx-4 align-items-center text-center">
+            <img src="../assets/sponsor-4-2x.png" alt="Power gym" />
+            <p class="my-3"><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i><i class="fa-solid fa-chevron-up"></i></p>
+            <p>Proin in viverra Maecenas et mattis ut nibh enim. Gravida ultricies mi porta lacus. Et posuere velit sed nec
+                eleifend</p>
+            <a class="my-3" href="#"> Visit Power Gym ></a>
+        </div>
 
-
+    </div>
 </template>
-<style scoped>
-.cardsponsor{
+<style lang="scss" scoped>
+.cardsponsor {
     margin-top: 50px;
     width: 250px;
     height: 300px;
-    img{
-        width: 150px;
+    img {
+        width: 60%;
+        height: 60%;
     }
 }
-
-a{
+i{
+    color: #4154ff;
+    margin: -3px;
+    font-size: 24px;
+}
+a {
     font-weight: bold;
     color: black;
+}
+.container{
+    height: 600px;
 }
 </style>
