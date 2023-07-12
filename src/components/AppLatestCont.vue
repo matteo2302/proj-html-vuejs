9cc6c3c274b2fb6df1a84c04938ce63c6d669767
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-12 19:45:03 +0200

    latest content

--- src/components/AppLatestCont.vue
+++ src/components/AppLatestCont.vue
@@ -1,29 +1,44 @@
 <script>
-export default{
+export default {
 }
 </script>
 <template>
-<div class="latest-cont my-5 container">
-    <div class="d-flex align-items-center justify-content-between"><h3>Latest Article</h3> <a href="#">Read all video ></a></div>    
-    <div class="videos d-flex row-cols-3 g-2">
-        <div class="col"><img src="../assets/blog4-2x.jpg" alt="video"><h4 class="ps-3">The best prootein shake</h4><a class="ps-3" href="#">By admin | November 26th,2019| Gym</a></div>
-        <div class="col"><img src="../assets/blog1-2x.jpg" alt="video"><h4 class="ps-3">Ultimate Arms workout</h4><a class="ps-3" href="#">By admin | November 26th,2019| Gym</a></div>
-        <div class="col"><img src="../assets/blog3-2x.jpg" alt="video"><h4 class="ps-3">New gears avaible now</h4><a class="ps-3" href="#">By admin | November 26th,2019| Gym</a></div>
+    <div class="latest-cont my-5 container">
+        <div class="d-flex align-items-center justify-content-between">
+            <h3>Latest Article</h3> <a href="#">Read all video ></a>
+        </div>
+        <div class="videos d-flex row-cols-3 justify-content-between">
+            <div class="col"><img src="../assets/blog4-2x.jpg" alt="video">
+                <h4 class="ps-3">The best prootein shake</h4><a class="ps-3" href="#">By admin | November 26th,2019| Gym</a>
+            </div>
+            <div class="col"><img src="../assets/blog1-2x.jpg" alt="video">
+                <h4 class="ps-3">Ultimate Arms workout</h4><a class="ps-3" href="#">By admin | November 26th,2019| Gym</a>
+            </div>
+            <div class="col"><img src="../assets/blog3-2x.jpg" alt="video">
+                <h4 class="ps-3">New gears avaible now</h4><a class="ps-3" href="#">By admin | November 26th,2019| Gym</a>
+            </div>
+        </div>
     </div>
-</div>
 </template>
-<style scoped>
-.latest-cont{
+<style lang="scss" scoped>
+.latest-cont {
     height: 450px;
 }
-.videos{
+
+.videos {
     height: 300px;
     margin-bottom: 100px;
-    img{
+
+    img {
         width: 100%;
     }
 }
-a{
-    color: black;
+
+a {
+    color: #98939F;
 }
-</style>
+
+.col {
+    width: 400px;
+
+}</style>
